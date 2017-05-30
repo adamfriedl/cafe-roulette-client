6853e0e5c90f9fe8e8a3074f8af75bfd1da415a9
Adam Friedl <ajfriedl@gmail.com>
2017-05-30 15:37:10 -0400

    added material-design-ui

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {WrapperApp} from './App';
-import './App.css'
+// import './App.css'
 
 const store = createStore(
   rootReducer,
@@ -15,9 +16,17 @@ const store = createStore(
   )
 )
 
+const App = () => (
+  <MuiThemeProvider>
+    <WrapperApp />
+  </MuiThemeProvider>
+)
+
+
+
 ReactDOM.render(
   <Provider store={store}>
-    <WrapperApp />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
