a74ba7543d51fcd2a05b2f42bfc8b073575e268b
Adam Friedl <ajfriedl@gmail.com>
2017-07-10 09:13:57 -0400

    restored fetch urls and removed Allow-Access header. Still getting CORS error on Heroku

--- src/actions/coffeeShopActions.js
+++ src/actions/coffeeShopActions.js
@@ -6,12 +6,7 @@ export function fetchCoffeeShops() {
     dispatch({type: 'LOADING_COFFEE_SHOPS'})
     return fetch(
       // 'http://localhost:9000/coffee_shops')
-    'https://floating-temple-82552.herokuapp.com', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin':'*'
-      }
-    })
+    'https://floating-temple-82552.herokuapp.com/coffee_shops')
       .then(res => res.json())
       .then(shops => dispatch({type: 'FETCH_COFFEE_SHOPS', payload: shops}))
   }
@@ -22,12 +17,7 @@ export function backgroundFetchCoffeeShops() {
   return function(dispatch) {
     return fetch(
       // 'http://localhost:9000/coffee_shops')
-    'https://floating-temple-82552.herokuapp.com', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin':'*'
-      }
-    })
+    'https://floating-temple-82552.herokuapp.com/coffee_shops')
       .then(res => res.json())
       .then(shops => dispatch({type: 'FETCH_COFFEE_SHOPS', payload: shops}))
   }
