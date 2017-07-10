b9924b2b7f16a04b432485e34e0754e354cd383f
Adam Friedl <ajfriedl@gmail.com>
2017-07-10 09:08:07 -0400

    adding Access-Control-Allow-Origin to fetch

--- src/actions/coffeeShopActions.js
+++ src/actions/coffeeShopActions.js
@@ -6,7 +6,12 @@ export function fetchCoffeeShops() {
     dispatch({type: 'LOADING_COFFEE_SHOPS'})
     return fetch(
       // 'http://localhost:9000/coffee_shops')
-    'https://floating-temple-82552.herokuapp.com')
+    'https://floating-temple-82552.herokuapp.com', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
       .then(res => res.json())
       .then(shops => dispatch({type: 'FETCH_COFFEE_SHOPS', payload: shops}))
   }
@@ -17,7 +22,12 @@ export function backgroundFetchCoffeeShops() {
   return function(dispatch) {
     return fetch(
       // 'http://localhost:9000/coffee_shops')
-    'https://floating-temple-82552.herokuapp.com')
+    'https://floating-temple-82552.herokuapp.com', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
       .then(res => res.json())
       .then(shops => dispatch({type: 'FETCH_COFFEE_SHOPS', payload: shops}))
   }
