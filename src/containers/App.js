d4ced1bf3b07dcd5a163004d9bdc6476672f0135
Adam Friedl <ajfriedl@gmail.com>
2017-11-22 15:45:44 -0500

    cleaning up code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,60 +1,60 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import PropTypes from 'prop-types'
-import * as actions from '../actions/coffeeShopActions'
-import CoffeeAppBar from '../components/CoffeeAppBar'
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
-import Recommendation from './Recommendation'
-import CoffeeShopsForm from './CoffeeShopsForm'
-import CoffeeShopsIndex from '../components/CoffeeShopsIndex'
-import Footer from '../components/Footer'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import * as actions from '../actions/coffeeShopActions';
+import CoffeeAppBar from '../components/CoffeeAppBar';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Recommendation from './Recommendation';
+import CoffeeShopsForm from './CoffeeShopsForm';
+import CoffeeShopsIndex from '../components/CoffeeShopsIndex';
+import Footer from '../components/Footer';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props)
-  //
-  //   this.state = {
-  //     // shops: this.props.shops,
-  //     // loading: this.props.loading
-  //   }
-  // }
-
   static propTypes = {
     loading: PropTypes.bool
-  }
+  };
 
   componentDidMount() {
-    this.props.actions.fetchCoffeeShops()
+    this.props.actions.fetchCoffeeShops();
   }
 
   render() {
-
     const appStyle = {
       fontFamily: 'Patrick Hand SC',
-      lineHeight: '1.2',
-    }
+      lineHeight: '1.2'
+    };
 
     const containerStyle = {
       marginLeft: 40,
       marginRight: 40,
       paddingTop: 64
-    }
+    };
 
     return (
       <div className="App" style={appStyle}>
-        <Router >
+        <Router>
           <div>
             <CoffeeAppBar />
             <div style={containerStyle}>
-              {this.props.loading
-                ? <h2>Loading...</h2>
-                : <Switch>
-                    <Route exact path={process.env.PUBLIC_URL + '/'} component={Recommendation} />
-                    <Route path='/coffeeshops/new' component={CoffeeShopsForm} />
-                    <Route path='/coffeeshops' render={props => <CoffeeShopsIndex shops={this.props.shops} />} />
-                  </Switch>
-              }
+              {this.props.loading ? (
+                <h2>Loading...</h2>
+              ) : (
+                <Switch>
+                  <Route
+                    exact
+                    path={process.env.PUBLIC_URL + '/'}
+                    component={Recommendation}
+                  />
+                  <Route path="/coffeeshops/new" component={CoffeeShopsForm} />
+                  <Route
+                    path="/coffeeshops"
+                    render={props => (
+                      <CoffeeShopsIndex shops={this.props.shops} />
+                    )}
+                  />
+                </Switch>
+              )}
               <Footer />
             </div>
           </div>
@@ -68,11 +68,11 @@ const mapStateToProps = state => {
   return {
     loading: state.loading,
     shops: state.shops
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-  return {actions: bindActionCreators(actions, dispatch)}
-}
+  return { actions: bindActionCreators(actions, dispatch) };
+};
 
-export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App)
+export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App);
--- src/containers/CoffeeShopsForm.js
+++ src/containers/CoffeeShopsForm.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as actions from '../actions/coffeeShopActions';
 import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
-import { ValidatorForm, TextValidator, SelectValidator} from 'react-material-ui-form-validator';
+import {
+  ValidatorForm,
+  TextValidator,
+  SelectValidator
+} from 'react-material-ui-form-validator';
 
 class CoffeeShopsForm extends Component {
-
   constructor() {
-    super()
+    super();
 
     this.state = {
       name: '',
@@ -19,55 +22,54 @@ class CoffeeShopsForm extends Component {
       food: '',
       price_range: '',
       votes: 0
-    }
+    };
   }
 
   componentDidMount() {
-    this.props.actions.resetSuccessHandler()
+    this.props.actions.resetSuccessHandler();
   }
 
   handleNameChange(e) {
     this.setState({
       name: e.target.value
     });
-  };
+  }
 
   handleDescriptionChange(e) {
     this.setState({
       description: e.target.value
     });
-  };
+  }
   handleWebsiteChange(e) {
     this.setState({
       website: e.target.value
     });
-  };
+  }
 
   handleAddressChange(e) {
     this.setState({
       address: e.target.value
     });
-  };
+  }
 
   handleFoodChange(e, index, value) {
     this.setState({
       food: value
     });
-  };
+  }
 
   handlePriceRangeChange(e, index, value) {
     this.setState({
       price_range: value
     });
-  };
+  }
 
   handleSubmit(e) {
-    e.preventDefault()
-    console.log(this.state)
-    this.props.actions.addCoffeeShop(this.state)
-      .then(
-        res => this.props.actions.backgroundFetchCoffeeShops()
-      )
+    e.preventDefault();
+    console.log(this.state);
+    this.props.actions
+      .addCoffeeShop(this.state)
+      .then(res => this.props.actions.backgroundFetchCoffeeShops());
 
     this.setState({
       name: '',
@@ -76,42 +78,37 @@ class CoffeeShopsForm extends Component {
       address: '',
       food: '',
       price_range: ''
-    })
+    });
 
-    window.scrollTo(0, 0)
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
   }
 
-
   render() {
-
     const flexContainer = {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center'
-    }
+    };
 
     const inputStyle = {
       fontFamily: 'Patrick Hand SC',
       fontSize: '1.3em'
-    }
+    };
 
-    return(
-      <div style={{fontFamily: 'Patrick Hand SC'}}>
-
+    return (
+      <div style={{ fontFamily: 'Patrick Hand SC' }}>
         <div style={flexContainer}>
           <h1>Add a Coffee Shop. You Can Do It!</h1>
         </div>
 
         <div style={flexContainer}>
-          <div className='message'>{this.props.message}</div>
+          <div className="message">{this.props.message}</div>
         </div>
 
         <div style={flexContainer}>
           <ValidatorForm
             name="CoffeeShopForm"
-            onSubmit={e => this.handleSubmit(e)}
-          >
-
+            onSubmit={e => this.handleSubmit(e)}>
             <TextValidator
               hintText="Central Perk"
               style={inputStyle}
@@ -146,9 +143,7 @@ class CoffeeShopsForm extends Component {
               value={this.state.website}
               floatingLabelText="Website"
               onChange={e => this.handleWebsiteChange(e)}
-              validators={[
-                'required'
-              ]}
+              validators={['required']}
               errorMessages={['This field is required, dude!']}
             />
             <br />
@@ -173,20 +168,12 @@ class CoffeeShopsForm extends Component {
               style={inputStyle}
               name="CoffeeShopFood"
               value={this.state.food}
-              onChange={(e, index, value) => this.handleFoodChange(e, index, value)}
+              onChange={(e, index, value) =>
+                this.handleFoodChange(e, index, value)}
               validators={['required']}
-              errorMessages={['This field is required, dude!']}
-            >
-              <MenuItem
-                value={true}
-                style={inputStyle}
-                primaryText="Ouais!"
-              />
-              <MenuItem
-                value={false}
-                style={inputStyle}
-                primaryText="Nein!"
-              />
+              errorMessages={['This field is required, dude!']}>
+              <MenuItem value={true} style={inputStyle} primaryText="Ouais!" />
+              <MenuItem value={false} style={inputStyle} primaryText="Nein!" />
             </SelectValidator>
             <br />
 
@@ -195,46 +182,45 @@ class CoffeeShopsForm extends Component {
               style={inputStyle}
               name="CoffeeShopPriceRange"
               value={this.state.price_range}
-              onChange={(e, index, value) => this.handlePriceRangeChange(e, index, value)}
+              onChange={(e, index, value) =>
+                this.handlePriceRangeChange(e, index, value)}
               validators={['required']}
-              errorMessages={['This field is required, dude!']}
-            >
+              errorMessages={['This field is required, dude!']}>
               <MenuItem
-                value={"$"}
+                value={'$'}
                 style={inputStyle}
                 primaryText="$-It's practically free"
               />
               <MenuItem
-                value={"$$"}
+                value={'$$'}
                 style={inputStyle}
                 primaryText="$$-Julian is buying!"
               />
-
-
             </SelectValidator>
             <br />
             <br />
 
             <FlatButton
-              style={{color: 'white'}}
+              style={{ color: 'white' }}
               backgroundColor="red"
               type="submit"
               label="Submit"
             />
-
           </ValidatorForm>
         </div>
       </div>
     );
   }
-};
+}
 
 const mapStateToProps = state => {
-  return {message: state.message}
-}
+  return { message: state.message };
+};
 
 const mapDispatchToProps = dispatch => {
-  return {actions: bindActionCreators(actions, dispatch)}
-}
+  return { actions: bindActionCreators(actions, dispatch) };
+};
 
-export default CoffeeShopsForm = connect(mapStateToProps, mapDispatchToProps)(CoffeeShopsForm);
+export default (CoffeeShopsForm = connect(mapStateToProps, mapDispatchToProps)(
+  CoffeeShopsForm
+));
