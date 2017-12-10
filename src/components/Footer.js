ffb6c4c12cd2d464c2e2c37feba9c7e259e8ca79
Adam Friedl <ajfriedl@gmail.com>
2017-12-10 11:33:25 -0500

    adjusted footer margins

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import WhatsHot from 'material-ui-icons/Whatshot'
+import React from 'react';
+import WhatsHot from 'material-ui-icons/Whatshot';
 
 const Footer = () => {
-
-let footerStyle = {
-    marginTop: 80,
+  let footerStyle = {
+    marginBottom: 40,
     textAlign: 'center'
-  }
+  };
 
   return (
     <div>
       <p style={footerStyle}>
-        Made with <WhatsHot color={'red'}/> by Adam Friedl
+        Made with <WhatsHot color={'red'} /> by Adam Friedl
       </p>
-
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
