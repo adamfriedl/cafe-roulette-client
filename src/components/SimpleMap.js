5f3ac62914769d87f04029fd02dcd1425ca44ba0
Adam Friedl <ajfriedl@gmail.com>
2017-12-11 21:50:55 -0500

    deleted unused component

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react'
-import GoogleMapReact from 'google-map-react'
-import MyCoffeeShop from './MyCoffeeShop'
-import StarBorder from 'material-ui-icons/StarBorder'
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import StarBorder from 'material-ui-icons/StarBorder';
 
 export default class SimpleMap extends Component {
   static defaultProps = {
-    center: {lat: 59.95, lng: 30.33},
+    center: { lat: 59.95, lng: 30.33 },
     zoom: 17
   };
 
   render() {
     return (
-      <GoogleMapReact
-        center={this.props.center}
-        zoom={this.props.zoom}
-      >
-        <StarBorder
-          color={'blue'}
-          lat={this.props.lat}
-          lng={this.props.lng}
-        />
-
+      <GoogleMapReact center={this.props.center} zoom={this.props.zoom}>
+        <StarBorder color={'blue'} lat={this.props.lat} lng={this.props.lng} />
       </GoogleMapReact>
     );
   }
