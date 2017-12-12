bee89e696419f924aeee4551a07d6997d75c86f3
Adam Friedl <ajfriedl@gmail.com>
2017-12-11 21:49:43 -0500

    added divider between shops

--- src/components/CoffeeShopsIndex.js
+++ src/components/CoffeeShopsIndex.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CoffeeShopDetailsIndex from './CoffeeShopDetailsIndex'
+import Divider from 'material-ui/Divider';
+import CoffeeShopDetailsIndex from './CoffeeShopDetailsIndex';
 
 const CoffeeShopsIndex = props => {
-
   // update(id) {
   //   fetch('http://localhost:9000/coffee_shops/' + id)
   //     .then(res => res.json())
@@ -10,39 +10,48 @@ const CoffeeShopsIndex = props => {
   //   )
   // }
 
-    let shops = props.shops.map(shop => (
-      <div key={shop.id}>
+  const flexContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+    // alignItems: 'center'
+  };
 
-        <h2><a href={shop.website}>{shop.name}</a></h2>
+  let shops = props.shops.map(shop => (
+    <div key={shop.id} style={flexContainer}>
+      <h2>
+        <a href={shop.website}>{shop.name}</a>
+      </h2>
 
-        <h3>Description</h3>
-        <p style={{marginTop: -15}}>{shop.description}</p>
+      <h3>Description</h3>
+      <p style={{ marginTop: -15 }}>{shop.description}</p>
 
-        <h3>Website</h3>
-        <p style={{marginTop: -15}}><a href={shop.website}>{shop.website}</a></p>
+      <h3>Website</h3>
+      <p style={{ marginTop: -15 }}>
+        <a href={shop.website}>{shop.website}</a>
+      </p>
 
-        <h3>Address</h3>
-        <p style={{marginTop: -15}}>{shop.address}</p>
+      <h3>Address</h3>
+      <p style={{ marginTop: -15 }}>{shop.address}</p>
 
-        <CoffeeShopDetailsIndex
-          shop={shop}
-        />
+      <CoffeeShopDetailsIndex shop={shop} />
+      <Divider style={{ marginTop: 20 }} />
+    </div>
+  ));
 
-      </div>
-    ))
-
-    return(
-      <div>
-        <h1>Every Single Awesome Shop</h1>
-        {shops}
-      </div>
-    )
+  return (
+    <div>
+      <h1>Every Single Awesome Shop</h1>
+      <Divider />
+      {shops}
+    </div>
+  );
   // }
-}
+};
 
 // const mapDispatchToProps = dispatch => {
 //   return {actions: bindActionCreators(actions, dispatch)}
 // }
 
 // export default CoffeeShopsIndex = connect(null, mapDispatchToProps)(CoffeeShopsIndex)
-export default CoffeeShopsIndex
+export default CoffeeShopsIndex;
