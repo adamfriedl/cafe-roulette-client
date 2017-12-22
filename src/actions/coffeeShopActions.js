3a512d65a7fe3d9a7a78facfd30fbf7f9161af28
Adam Friedl <ajfriedl@gmail.com>
2017-12-22 10:42:45 -0500

    deleted unused dev urls

--- src/actions/coffeeShopActions.js
+++ src/actions/coffeeShopActions.js
@@ -1,34 +1,25 @@
-import fetch from 'isomorphic-fetch'
+import fetch from 'isomorphic-fetch';
 
 export function fetchCoffeeShops() {
-
   return function(dispatch) {
-    dispatch({type: 'LOADING_COFFEE_SHOPS'})
-    return fetch(
-    // 'http://localhost:9000/coffee_shops')
-    'https://floating-temple-82552.herokuapp.com/coffee_shops')
+    dispatch({ type: 'LOADING_COFFEE_SHOPS' });
+    return fetch('https://floating-temple-82552.herokuapp.com/coffee_shops')
       .then(res => res.json())
-      .then(shops => dispatch({type: 'FETCH_COFFEE_SHOPS', payload: shops}))
-  }
+      .then(shops => dispatch({ type: 'FETCH_COFFEE_SHOPS', payload: shops }));
+  };
 }
 
 export function backgroundFetchCoffeeShops() {
-
   return function(dispatch) {
-    return fetch(
-    // 'http://localhost:9000/coffee_shops')
-    'https://floating-temple-82552.herokuapp.com/coffee_shops')
+    return fetch('https://floating-temple-82552.herokuapp.com/coffee_shops')
       .then(res => res.json())
-      .then(shops => dispatch({type: 'FETCH_COFFEE_SHOPS', payload: shops}))
-  }
+      .then(shops => dispatch({ type: 'FETCH_COFFEE_SHOPS', payload: shops }));
+  };
 }
 
 export function addCoffeeShop(data) {
-
   return function(dispatch) {
-    return fetch(
-      // 'http://localhost:9000/coffee_shops', {
-      'https://floating-temple-82552.herokuapp.com/coffee_shops', {
+    return fetch('https://floating-temple-82552.herokuapp.com/coffee_shops', {
       method: 'POST',
       mode: 'CORS',
       headers: {
@@ -36,36 +27,20 @@ export function addCoffeeShop(data) {
       },
       body: JSON.stringify(data)
     }).then(
-      res => dispatch({type: 'SUBMIT_FORM_SUCCESS', payload: res}),
-      error => dispatch({type: 'SUBMIT_FORM_FAILURE', error: error,
-      suppressGlobalErrorNotification: (
-        error.response &&
-        error.response.status === 400
-      )
-    }))
-  }
+      res => dispatch({ type: 'SUBMIT_FORM_SUCCESS', payload: res }),
+      error =>
+        dispatch({
+          type: 'SUBMIT_FORM_FAILURE',
+          error: error,
+          suppressGlobalErrorNotification:
+            error.response && error.response.status === 400
+        })
+    );
+  };
 }
 
-// export function upVote(id, voteTicker) {
-
-//   return function(dispatch) {
-//     return fetch('http://localhost:9000/coffee_shops/' + id, {
-//       method: 'PATCH',
-//       mode: 'CORS',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({votes: voteTicker})
-//     }).then(res => res.json())
-//       .then(shop => console.log(shop))
-//       .then(shop => dispatch({type: 'UPVOTE_SHOP', id, votes: voteTicker}))
-//
-//   }
-// }
-
 export function resetSuccessHandler() {
-
   return function(dispatch) {
-    dispatch({type: 'RESET_SUCCESS_HANDLER'})
-  }
+    dispatch({ type: 'RESET_SUCCESS_HANDLER' });
+  };
 }
